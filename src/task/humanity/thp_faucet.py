6441bf73094e88e1795f7e3665303cb9827ddfa7
yunlong <819243476@qq.com>
2024-11-12 14:47:49 +0800

    add thp faucet

--- src/task/humanity/thp_faucet.py
+++ src/task/humanity/thp_faucet.py
@@ -13,7 +13,7 @@ except NameError as e:
     print('NameError:', e)
 else:
     a1 = param_map['account_1']
-    address = a1.split(',')[0]
+    address = a1.address
     exe_num = param_map['exe_num']
     instanceId = param_map['instance_id']
     proxy_ip_str = param_map['proxy_ip']
@@ -25,22 +25,8 @@ user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Ge
 
 
 
-def btc_faucet(w):
-    address = 'tb1pkazmlpyfaeuu78chfyn8t5073healy2vgh9dyapev9ruc9khumxqcdv2sq'
 
-    url = 'https://api.thefaucet.org/v3/is_not_login_basic_claimable?coin_config_id=1&user_address=' + address
-
-    rsp = w.session.request(method='get', url=url)
-
-
-    log_msg = f"[{batch_name}][{exe_num}][{instanceId}]  {address} : status code: {rsp.status_code} content: {rsp.json()}"
-    worker_log.msg_info(log_msg, instanceId)
-
-    pass
-
-
-
-def claim_btc_test(address):
+def claim_thp_test(address):
     from src.utils.clash_proxy import ClashProxy
     clash = ClashProxy()
     worker_log.msg_info(f"[{batch_name}][{exe_num}][{instanceId}]  {address} : [{clash.clash_url}]..start.", instanceId)
@@ -53,10 +39,9 @@ def claim_btc_test(address):
         clash.change_node(nodes[exe_num])
 
     w = Web3Wrap(block_chain=Block_chain.Sepolia, proxy_ip=proxy_ip_str, gas_flag=False)
-    url = 'https://api.thefaucet.org/v3/claim_basic_not_login'
+    url = 'https://faucet.testnet.humanity.org/api/claim'
 
     payload = {
-        "coin_id": 1,
         "address": address
     }
     headers = {
@@ -71,4 +56,4 @@ def claim_btc_test(address):
 
 
 
-claim_btc_test(address)
+claim_thp_test(address)
